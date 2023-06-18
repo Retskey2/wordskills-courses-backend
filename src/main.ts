16a89c03c5e208ea0d9ac9968141bbf1fce78e9f
RETSKEY <bashlovka96@mail.ru>
2023-06-18 12:08:22 +0300

    fix #6

--- src/main.ts
+++ src/main.ts
@@ -11,11 +11,11 @@ async function bootstrap() {
 
   app.enableCors({
     origin: true,
-    methods: 'GET, PUT, POST, DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    credentials: true,
 })
   
-
   if (!configService.isProduction()) {
     const document = SwaggerModule.createDocument(app, new DocumentBuilder()
       .setTitle('Item API')
